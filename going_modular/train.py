95348536962b1d3c8eb4a8160244d76845f2dcfd
Meuky <841463443@qq.com>
2026-01-20 22:49:30 +0800

    05_going_modual completed

---
+++ going_modular/train.py
@@ -0,0 +1,76 @@
+"""
+Trains a PyTorch image classification model using device-agnostic code.
+"""
+
+import os
+import torch
+import data_setup, engine, model_builder, utils
+import argparse 
+from torchvision import transforms
+
+def parse_args():
+    """解析命令行传入的超参数"""
+    parser = argparse.ArgumentParser(description="训练pizza_steak_sushi分类模型")
+    
+    parser.add_argument("--learning_rate", 
+                        type=float, 
+                        default=0.001)
+    
+    parser.add_argument("--batch_size", 
+                        type=int, 
+                        default=32)
+    
+    parser.add_argument("--num_epochs", 
+                        type=int, 
+                        default=10)
+    
+    parser.add_argument("--hidden_units", 
+                        type=int, 
+                        default=10)
+    
+    return parser.parse_args()
+
+
+args = parse_args()
+
+NUM_EPOCHS = args.num_epochs      
+BATCH_SIZE = args.batch_size       
+HIDDEN_UNITS = args.hidden_units   
+LEARNING_RATE = args.learning_rate
+
+train_dir = "data/pizza_steak_sushi/train"
+test_dir = "data/pizza_steak_sushi/test"
+
+device = "mps" if torch.backends.mps.is_available() else "cpu"
+
+data_transform = transforms.Compose([transforms.Resize((64, 64)),
+                                     transforms.ToTensor()])
+
+train_dataloader, test_dataloader, class_names = data_setup.create_dataloaders(
+    train_dir=train_dir,
+    test_dir=test_dir,
+    transform=data_transform,
+    batch_size=BATCH_SIZE
+    )
+
+model = model_builder.TinyVGG(
+    input_shape=3,
+    hidden_units=HIDDEN_UNITS,
+    output_shape=len(class_names)
+    ).to(device)
+
+loss_fn = torch.nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(),
+                             lr=LEARNING_RATE)
+
+engine.train(model=model,
+             train_dataloader=train_dataloader,
+             test_dataloader=test_dataloader,
+             loss_fn=loss_fn,
+             optimizer=optimizer,
+             epochs=NUM_EPOCHS,
+             device=device)
+
+utils.sav_model(model=model,
+                target_dir="models",
+                model_name="05_going_modual_tinyvgg_model.pth")
